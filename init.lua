4e1ba6832ad22ffe61bf82f225debefa52cc00e1
aviditguy <133472860+aviditguy@users.noreply.github.com>
2024-08-16 12:36:00 +0530

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,18 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.cursorline = true
+vim.opt.whichwrap:append("<,>,h,l")
+
+-- render leading whitespace
+vim.opt.list = true
+vim.opt.listchars = {tab="⋅ ", lead="⋅"}
+
+vim.keymap.set("n", "<", "<<", {noremap=true, silent=true})
+vim.keymap.set("n", ">", ">>", {noremap=true, silent=true})
