2e87a64e4b5f7e502360562cec9a07edc9a74667
aviditguy <133472860+aviditguy@users.noreply.github.com>
2024-12-10 03:41:52 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 3
+vim.opt.shiftwidth = 3
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
