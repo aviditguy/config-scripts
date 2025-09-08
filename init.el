de25f74a669172c61033f145313cf09a63fd28a6
aviditguy <133472860+aviditguy@users.noreply.github.com>
2025-09-08 13:52:33 +0530

    Update init.el

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
-;; ===============================
+;; ===========================================================================================
 ;; PACKAGE MANAGEMENT
-;; ===============================
+;; ===========================================================================================
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("gnu" . "https://elpa.gnu.org/packages/")))
@@ -11,9 +11,9 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; ===============================
+;; ===========================================================================================
 ;; BASIC SETTINGS
-;; ===============================
+;; ===========================================================================================
 (setq inhibit-startup-message t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -23,8 +23,7 @@
 (setq display-line-numbers-type 'relative)
 (ido-mode 1)
 (ido-everywhere 1)
-;;(electric-pair-mode 1)
-(set-face-attribute 'default nil :font "Monospace" :height 150)
+(set-face-attribute 'default nil :font "Iosevka ExtraLight Extended" :height 150)
 
 (setq auto-save-default nil)   ;; Disable auto-saving
 (setq make-backup-files nil)   ;; Disable backup~ files
@@ -40,28 +39,11 @@
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-;; Choose Theme using F5
 (load-theme 'doom-material-dark t)
 
-(defvar my/all-themes (custom-available-themes))
-(defvar my/current-theme-index -1)
-
-(defun my/cycle-all-themes ()
-  "Disable current theme and load the next available one."
-  (interactive)
-  ;; Disable any currently enabled themes
-  (mapc #'disable-theme custom-enabled-themes)
-  ;; Increment index
-  (setq my/current-theme-index (% (1+ my/current-theme-index) (length my/all-themes)))
-  ;; Load next theme
-  (load-theme (nth my/current-theme-index my/all-themes) t)
-  (message "Loaded theme: %s" (nth my/current-theme-index my/all-themes)))
-
-(global-set-key (kbd "<f5>") 'my/cycle-all-themes)
-
-;; =====================================================================
+;; ===========================================================================================
 ;; Formatting
-;; =====================================================================
+;; ===========================================================================================
 (defun format-current-buffer ()
   "Format current buffer with clang-format (for C/C++) or black (for Python)."
   (interactive)
@@ -76,12 +58,30 @@
 
 (global-set-key (kbd "C-c f") 'format-current-buffer)
 
-;; =====================================================================
-;; Org Setup
-;; =====================================================================
-(setq org-hide-emphasis-markers t)
+;; ===========================================================================================
+;; C
+;; ===========================================================================================
+(use-package lsp-mode
+  :hook ((c-mode c++-mode) . lsp)
+  :commands lsp)
 
+(use-package lsp-ui :commands lsp-ui-mode)
+
+(use-package company
+  :config
+  (global-company-mode))  ;; autocomplete
+
+(use-package flycheck
+  :init (global-flycheck-mode)) ;; inline errors
+
+;; ===========================================================================================
+;; Org Setup
+;; ===========================================================================================
+(setq org-hide-emphasis-markers t)
 (setq org-startup-folded 'overview)
+
+(setq org-indent-indentation-per-level 3)
+(add-hook 'org-mode-hook 'org-indent-mode)
 
 (font-lock-add-keywords 'org-mode
                           '(("^ *\\([-]\\) "
@@ -92,12 +92,15 @@
     (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
 (custom-set-faces
- '(org-level-1 ((t (:inherit default :weight normal
-			     :height 1.6))))
- '(org-level-2 ((t (:inherit default :height 1.5))))
- '(org-level-3 ((t (:inherit default :height 1.4))))
- '(org-level-4 ((t (:inherit default :height 1.3))))
- '(org-level-5 ((t (:inherit default :height 1.2))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit default :weight normal :height 1.5))))
+ '(org-level-2 ((t (:inherit default :height 1.4))))
+ '(org-level-3 ((t (:inherit default :height 1.3))))
+ '(org-level-4 ((t (:inherit default :height 1.2))))
+ '(org-level-5 ((t (:inherit default :height 1.1))))
  '(org-level-6 ((t (:inherit default :height 1.1))))
  '(org-level-7 ((t (:inherit default :height 1.1))))
  '(org-level-8 ((t (:inherit default :height 1.1)))))
@@ -106,16 +109,58 @@
 (add-to-list 'org-file-apps '("\\.mp4\\'" . "mpv %s"))
 (add-to-list 'org-file-apps '("\\.mkv\\'" . "mpv %s"))
 
+;; Default org-babel header args for Python
+(setq org-babel-default-header-args:python
+      '((:results . "output")))
+
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((emacs-lisp . t)))
+ '((emacs-lisp . t)
+   (shell      . t)
+   (C          . t)
+   (python     . t)))
 
 (setq org-confirm-babel-evaluate nil)
 
+;; ===========================================================================================
+;; Evil Mode (Vim keybindings)
+;; ===========================================================================================
+(use-package evil
+  :init
+  (setq evil-want-integration t) ;; required by evil-collection
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)  ;; let C-u scroll up like in Vim
+  (setq evil-want-C-i-jump nil)  ;; avoid TAB issues
+  :config
+  (evil-mode 1)
 
-;; =====================================================================
+  ;; Restore Emacs-like keybindings in insert & normal state
+  (define-key evil-insert-state-map (kbd "C-a") #'move-beginning-of-line)
+  (define-key evil-insert-state-map (kbd "C-e") #'move-end-of-line)
+  (define-key evil-insert-state-map (kbd "C-n") #'next-line)
+  (define-key evil-insert-state-map (kbd "C-p") #'previous-line)
+)
+
+;; (use-package evil-numbers
+;;   :after evil
+;;   :config
+;;   (define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
+;;   (define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt))
+
+(use-package evil-commentary
+  :after evil
+  :config
+  (evil-commentary-mode))
+
+;; Optional: Better Evil integration across modes
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+;; ===========================================================================================
 ;; LaTeX Previews
-;; =====================================================================
+;; ===========================================================================================
 (with-eval-after-load 'org
   (setq org-preview-latex-default-process 'dvisvgm)
   ;; Bigger previews
@@ -126,3 +171,9 @@
 (use-package org-fragtog
   :after org
   :hook (org-mode . org-fragtog-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
